66dd74176f0023248ae66477e6ee5ebfe4746221
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:40 -0300

    chore: re-index ../nimpkgs/p/procs

--- v0_2_1/flake.nix
+++ v0_2_1/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-procs-v0_2_1.flake = false;
   inputs.src-procs-v0_2_1.owner = "c-blake";
-  inputs.src-procs-v0_2_1.ref   = "refs/tags/v0.2.1";
+  inputs.src-procs-v0_2_1.ref   = "v0_2_1";
   inputs.src-procs-v0_2_1.repo  = "procs";
   inputs.src-procs-v0_2_1.type  = "github";
   
   inputs."cligen".owner = "nim-nix-pkgs";
   inputs."cligen".ref   = "master";
   inputs."cligen".repo  = "cligen";
+  inputs."cligen".dir   = "v1_5_22";
   inputs."cligen".type  = "github";
   inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
   inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
